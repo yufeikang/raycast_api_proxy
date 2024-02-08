5eb1aadb4856217fc02265475f4e8c8fea3a69c7
Yufei Kang <kou.uhi.x@gmail.com>
2024-02-08 19:01:42 +0900

    feat: (main) Refactor and add ChatBot functionality with OpenAI and Google API support
    >sdb

--- app/main.py
+++ app/main.py
@@ -1,15 +1,14 @@
+import abc
 import json
 import logging
 import os
-from itertools import chain
 from pathlib import Path
-
 
 import httpx
 import openai
-from google import generativeai as genai
 from fastapi import FastAPI, Request, Response
 from fastapi.responses import StreamingResponse
+from google import generativeai as genai
 
 from app.utils import ProxyRequest, pass_through_request
 
@@ -29,6 +28,213 @@ ALLOWED_USERS = (
 )
 
 MAX_TOKENS = os.environ.get("MAX_TOKENS", 1024)
+
+
+class ChatBotAbc(abc.ABC):
+
+    @classmethod
+    @abc.abstractmethod
+    def is_start_available(cls):
+        pass
+
+    @abc.abstractmethod
+    def chat_completions(self, raycast_data: dict):
+        pass
+
+    def get_models(self, raycast_data: dict):
+        pass
+
+
+class OpenAIChatBot(ChatBotAbc):
+
+    @classmethod
+    def is_start_available(cls):
+        return os.environ.get("OPENAI_API_KEY") or os.environ.get(
+            "AZURE_OPENAI_API_KEY"
+        )
+
+    def __init__(self) -> None:
+        super().__init__()
+        openai.api_key = os.environ.get("OPENAI_API_KEY") or os.environ.get(
+            "AZURE_OPENAI_API_KEY"
+        )
+        is_azure = openai.api_type in ("azure", "azure_ad", "azuread")
+        if is_azure:
+            logger.info("Using Azure API")
+            self.openai_client = openai.AsyncAzureOpenAI(
+                azure_endpoint=os.environ.get("OPENAI_AZURE_ENDPOINT"),
+                azure_ad_token_provider=os.environ.get("AZURE_DEPLOYMENT_ID", None),
+            )
+        else:
+            logger.info("Using OpenAI API")
+            self.openai_client = openai.AsyncOpenAI()
+
+    def __build_openai_messages(self, raycast_data: dict):
+        openai_messages = []
+        temperature = os.environ.get("TEMPERATURE", 0.5)
+        for msg in raycast_data["messages"]:
+            if "system_instructions" in msg["content"]:
+                openai_messages.append(
+                    {
+                        "role": "system",
+                        "content": msg["content"]["system_instructions"],
+                    }
+                )
+            if "command_instructions" in msg["content"]:
+                openai_messages.append(
+                    {
+                        "role": "system",
+                        "content": msg["content"]["command_instructions"],
+                    }
+                )
+            if "additional_system_instructions" in raycast_data:
+                openai_messages.append(
+                    {
+                        "role": "system",
+                        "content": raycast_data["additional_system_instructions"],
+                    }
+                )
+            if "text" in msg["content"]:
+                openai_messages.append(
+                    {"role": "user", "content": msg["content"]["text"]}
+                )
+            if "temperature" in msg["content"]:
+                temperature = msg["content"]["temperature"]
+        return openai_messages, temperature
+
+    async def chat_completions(self, raycast_data: dict):
+        openai_messages, temperature = self.__build_openai_messages(raycast_data)
+        try:
+            stream = await self.openai_client.chat.completions.create(
+                model=raycast_data["model"],
+                messages=openai_messages,
+                max_tokens=MAX_TOKENS,
+                n=1,
+                temperature=temperature,
+                stream=True,
+            )
+        except openai.OpenAIError as e:
+            logger.error(f"OpenAI error: {e}")
+            yield f'data: {json.dumps({"text": e.message, "finish_reason": "error"})}\n\n'
+            return
+        async for chunk in stream:
+            choice = chunk.choices[0]
+            logger.debug(f"OpenAI response chunk: {choice.delta.content}")
+            if choice.finish_reason is not None:
+                yield f'data: {json.dumps({"text": "", "finish_reason": choice.finish_reason})}\n\n'
+            if choice.delta and choice.delta.content:
+                yield f'data: {json.dumps({"text": choice.delta.content})}\n\n'
+
+    def get_models(self, raycast_data: dict):
+        default_models = {
+            "chat": "openai-gpt-3.5-turbo",
+            "quick_ai": "openai-gpt-3.5-turbo",
+            "commands": "openai-gpt-3.5-turbo",
+            "api": "openai-gpt-3.5-turbo",
+        }
+        models = [
+            {
+                "id": "openai-gpt-3.5-turbo",
+                "model": "gpt-3.5-turbo",
+                "name": "GPT-3.5 Turbo",
+                "provider": "openai",
+                "provider_name": "OpenAI",
+                "requires_better_ai": True,
+                "features": [
+                    "chat",
+                    "quick_ai",
+                    "commands",
+                    "api",
+                ],
+            },
+            {
+                "id": "openai-gpt-4-1106-preview",
+                "model": "gpt-4-1106-preview",
+                "name": "GPT-4 Turbo",
+                "provider": "openai",
+                "provider_name": "OpenAI",
+                "requires_better_ai": True,
+                "features": [
+                    "chat",
+                    "quick_ai",
+                    "commands",
+                    "api",
+                ],
+            },
+        ]
+        return {"default_models": default_models, "models": models}
+
+
+class GeminiChatBot(ChatBotAbc):
+    def __init__(self) -> None:
+        super().__init__()
+        logger.info("Using Google API")
+        google_api_key = os.environ.get("GOOGLE_API_KEY")
+        self.genai_client = genai.configure(api_key=google_api_key)
+
+    async def chat_completions(self, raycast_data: dict):
+        model = self.genai_client.GenerativeModel(raycast_data["model"])
+        google_message = ""
+        temperature = os.environ.get("TEMPERATURE", 0.5)
+        for msg in raycast_data["messages"]:
+            if "system_instructions" in msg["content"]:
+                google_message += msg["content"]["system_instructions"] + "\n"
+            if "command_instructions" in msg["content"]:
+                google_message += msg["content"]["command_instructions"] + "\n"
+            if "additional_system_instructions" in raycast_data:
+                google_message += raycast_data["additional_system_instructions"] + "\n"
+            if "text" in msg["content"]:
+                google_message += msg["content"]["text"] + "\n"
+            if "temperature" in msg["content"]:
+                temperature = msg["content"]["temperature"]
+
+        logger.debug(f"text: {google_message}")
+        result = await model.generate_content_async(
+            google_message,
+            stream=True,
+            generation_config=self.genai_client.types.GenerationConfig(
+                candidate_count=1,
+                max_output_tokens=MAX_TOKENS,
+                temperature=temperature,
+            ),
+        )
+        try:
+            async for chunk in result:
+                logger.debug(f"Gemini response chunk: {chunk.text}")
+                yield f'data: {json.dumps({"text": chunk.text})}\n\n'
+        except genai.types.BlockedPromptException as e:
+            logger.debug(f"Gemini response finish: {e}")
+            yield f'data: {json.dumps({"text": "", "finish_reason": e})}\n\n'
+
+    def get_models(self, raycast_data: dict):
+        default_models = {
+            "chat": "gemini-pro",
+            "quick_ai": "gemini-pro",
+            "commands": "gemini-pro",
+            "api": "gemini-pros",
+        }
+        models = [
+            {
+                "id": "gemini-pro",
+                "model": "gemini-pro",
+                "name": "Gemini Pro",
+                "provider": "google",
+                "provider_name": "Google",
+                "requires_better_ai": True,
+                "features": [
+                    "chat",
+                    "quick_ai",
+                    "commands",
+                    "api",
+                ],
+            }
+        ]
+        return {"default_models": default_models, "models": models}
+
+
+AI_INSTANCE: ChatBotAbc = (
+    OpenAIChatBot() if OpenAIChatBot.is_start_available() else GeminiChatBot()
+)
 
 
 def add_user(request: Request, user_email: str):
@@ -58,199 +264,17 @@ async def shutdown_event():
     await http_client.aclose()
 
 
-FORCE_MODEL = os.environ.get("FORCE_MODEL", None)
-
-SERVICE_PROVIDERS = {
-    "openai": [
-        {
-            "id": "openai-gpt-3.5-turbo",
-            "model": "gpt-3.5-turbo",
-            "name": "GPT-3.5 Turbo",
-            "provider": "openai",
-            "provider_name": "OpenAI",
-            "requires_better_ai": True,
-            "features": [
-                "chat",
-                "quick_ai",
-                "commands",
-                "api",
-            ],
-        },
-        {
-            "id": "openai-gpt-4-1106-preview",
-            "model": "gpt-4-1106-preview",
-            "name": "GPT-4 Turbo",
-            "provider": "openai",
-            "provider_name": "OpenAI",
-            "requires_better_ai": True,
-            "features": [
-                "chat",
-                "quick_ai",
-                "commands",
-                "api",
-            ],
-        },
-    ],
-    "google": [
-        {
-            "id": "gemini-pro",
-            "model": "gemini-pro",
-            "name": "Gemini Pro",
-            "provider": "google",
-            "provider_name": "Google",
-            "requires_better_ai": True,
-            "features": [],
-        },
-    ],
-}
-
-MODEL_PROVIDER_MAP = {
-    p["model"]: p["provider"] for p in chain.from_iterable(SERVICE_PROVIDERS.values())
-}
-
-openai_api_key = os.environ.get("OPENAI_API_KEY")
-google_api_key = os.environ.get("GOOGLE_API_KEY")
-if google_api_key:
-    genai.configure(api_key=google_api_key)
-    logger.info("Using Google API")
-
-    RAYCAST_DEFAULT_MODELS = {
-        "chat": "gemini-pro",
-        "quick_ai": "gemini-pro",
-        "commands": "gemini-pro",
-        "api": "gemini-pros",
-    }
-elif openai_api_key:
-    openai.api_key = openai_api_key
-    is_azure = openai.api_type in ("azure", "azure_ad", "azuread")
-    if is_azure:
-        logger.info("Using Azure API")
-        openai_client = openai.AzureOpenAI(
-            azure_endpoint=os.environ.get("OPENAI_AZURE_ENDPOINT"),
-            azure_ad_token_provider=os.environ.get("AZURE_DEPLOYMENT_ID", None),
-        )
-    else:
-        logger.info("Using OpenAI API")
-        openai_client = openai.OpenAI()
-
-    RAYCAST_DEFAULT_MODELS = {
-        "chat": "openai-gpt-3.5-turbo",
-        "quick_ai": "openai-gpt-3.5-turbo",
-        "commands": "openai-gpt-3.5-turbo",
-        "api": "openai-gpt-3.5-turbo",
-    }
-
-
-def get_model(raycast_data: dict):
-    return FORCE_MODEL or raycast_data["model"]
-
-
-async def chat_completions_openai(raycast_data: dict):
-    openai_messages = []
-    temperature = os.environ.get("TEMPERATURE", 0.5)
-    for msg in raycast_data["messages"]:
-        if "system_instructions" in msg["content"]:
-            openai_messages.append(
-                {
-                    "role": "system",
-                    "content": msg["content"]["system_instructions"],
-                }
-            )
-        if "command_instructions" in msg["content"]:
-            openai_messages.append(
-                {
-                    "role": "system",
-                    "content": msg["content"]["command_instructions"],
-                }
-            )
-        if "additional_system_instructions" in raycast_data:
-            openai_messages.append(
-                {
-                    "role": "system",
-                    "content": raycast_data["additional_system_instructions"],
-                }
-            )
-        if "text" in msg["content"]:
-            openai_messages.append({"role": "user", "content": msg["content"]["text"]})
-        if "temperature" in msg["content"]:
-            temperature = msg["content"]["temperature"]
-
-    def openai_stream():
-        stream = openai_client.chat.completions.create(
-            model=get_model(raycast_data),
-            messages=openai_messages,
-            max_tokens=MAX_TOKENS,
-            n=1,
-            stop=None,
-            temperature=temperature,
-            stream=True,
-        )
-        for response in stream:
-            chunk = response.choices[0]
-            if chunk.finish_reason is not None:
-                logger.debug(f"OpenAI response finish: {chunk.finish_reason}")
-                yield f'data: {json.dumps({"text": "", "finish_reason": chunk.finish_reason})}\n\n'
-            if chunk.delta and chunk.delta.content:
-                logger.debug(f"OpenAI response chunk: {chunk.delta.content}")
-                yield f'data: {json.dumps({"text": chunk.delta.content})}\n\n'
-
-    return StreamingResponse(openai_stream(), media_type="text/event-stream")
-
-
-async def chat_completions_gemini(raycast_data: dict):
-    model = genai.GenerativeModel(get_model(raycast_data))
-
-    google_message = ""
-    temperature = os.environ.get("TEMPERATURE", 0.5)
-    for msg in raycast_data["messages"]:
-        if "system_instructions" in msg["content"]:
-            google_message += msg["content"]["system_instructions"] + "\n"
-        if "command_instructions" in msg["content"]:
-            google_message += msg["content"]["command_instructions"] + "\n"
-        if "additional_system_instructions" in raycast_data:
-            google_message += raycast_data["additional_system_instructions"] + "\n"
-        if "text" in msg["content"]:
-            google_message += msg["content"]["text"] + "\n"
-        if "temperature" in msg["content"]:
-            temperature = msg["content"]["temperature"]
-
-    logger.debug(f"text: {google_message}")
-    result = model.generate_content(
-        google_message,
-        stream=True,
-        generation_config=genai.types.GenerationConfig(
-            candidate_count=1,
-            max_output_tokens=MAX_TOKENS,
-            temperature=temperature,
-        ),
-    )
-
-    def gemini_stream():
-        try:
-            for chunk in result:
-                logger.debug(f"Gemini response chunk: {chunk.text}")
-                yield f'data: {json.dumps({"text": chunk.text})}\n\n'
-        except genai.types.BlockedPromptException as e:
-            logger.debug(f"Gemini response finish: {e}")
-            yield f'data: {json.dumps({"text": "", "finish_reason": e})}\n\n'
-
-    return StreamingResponse(gemini_stream(), media_type="text/event-stream")
-
-
 @app.post("/api/v1/ai/chat_completions")
 async def chat_completions(request: Request):
     raycast_data = await request.json()
     if not check_auth(request):
         return Response(status_code=401)
-    logger.info(f"Received chat completion request: {raycast_data}")
+    logger.info("Received chat completion request")
 
-    model_id = get_model(raycast_data)
-    logger.debug(f"Use model id: {model_id}")
-
-    if MODEL_PROVIDER_MAP[model_id] == "openai" and openai_api_key:
-        return await chat_completions_openai(raycast_data)
-    if MODEL_PROVIDER_MAP[model_id] == "google" and google_api_key:
-        return await chat_completions_gemini(raycast_data)
+    return StreamingResponse(
+        AI_INSTANCE.chat_completions(raycast_data=raycast_data),
+        media_type="text/event-stream",
+    )
 
 
 @app.api_route("/api/v1/me", methods=["GET"])
@@ -304,8 +328,7 @@ async def proxy_models(request: Request):
     content = response.content
     if response.status_code == 200:
         data = json.loads(content)
-        data["default_models"] = RAYCAST_DEFAULT_MODELS
-        data["models"] = list(chain.from_iterable(SERVICE_PROVIDERS.values()))
+        data.update(AI_INSTANCE.get_models(data))
         content = json.dumps(data, ensure_ascii=False).encode("utf-8")
     return Response(
         status_code=response.status_code,
